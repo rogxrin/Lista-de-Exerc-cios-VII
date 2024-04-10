fc0b34652e75505364b36dacd1f46aaec2daf6a2
Rogério Lopes <126629082+rogxrin@users.noreply.github.com>
2024-04-10 20:19:54 -0300

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Main {
+    public static boolean isPalindrome(String word) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
